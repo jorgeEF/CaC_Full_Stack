fbcbfbb3a9a0408d252b49a1a31140d11d913b6f
jorgeEF <jorgefemenia@gmail.com>
2022-10-04 10:37:03 -0300

    agregadas tipos de funciones

--- 4.JAVASCRIPT/Ejercicios/e1/js/script6.js
+++ 4.JAVASCRIPT/Ejercicios/e1/js/script6.js
@@ -11,12 +11,21 @@ function saludarNombre(nombre){
 }
 console.log(saludarNombre("Jorge"));
 
+// función declarada
 function sumar(a, b){
     return a + b;
 }
 console.log(sumar(5, 3));
 
+// función expresada
 let sumarOtraForma = function(a,b){
     return a + b;
 }
 console.log(sumarOtraForma(5, 3));
+
+// función flecha
+let sumarFlecha = (a, b) => a + b;
+
+let sumarFlechaAmpliada = (a, b) => {
+    return a + b;
+}
