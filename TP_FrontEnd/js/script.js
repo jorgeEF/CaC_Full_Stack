a15ff350fd7712b0d9d186aed5b37196ebb2fa3f
jorgeEF <jorgefemenia@gmail.com>
2022-10-11 17:30:57 -0300

    avance codigo javascript

--- TP_FrontEnd/js/script.js
+++ TP_FrontEnd/js/script.js
@@ -1,39 +1,36 @@
 // obtener elementos del HTML por id
-let btnResumen = document.getElementById("botonResumen");
-let calculo = document.getElementById("mostrarCalculo");
-let texto = document.createElement("texto");
-let cantidad = document.getElementById("cantidad");
-let categoria = document.getElementById("categoria");
-let total;
+const btnResumen = document.getElementById("botonResumen");
+const campoTotal = document.getElementById("campoTotal");
 
 // funcion
 function calcular(cant, cat){
     let ticket = 200;
-    let descuento = 0;
+    let descuento;
     switch(cat){
-        case "Estudiante":
+        case 'Estudiante':
             descuento= 0.8;
             return cant * (ticket*descuento);
         break;
-        case "Trainee":
+        case 'Trainee':
             descuento= 0.5;
             return cant * (ticket*descuento);
         break;
-        case "Junior":
+        case 'Junior':
             descuento= 0.15;
             return cant * (ticket*descuento);
         break;        
     }
     
 }
+
+function mostrar(){
+    let cantidad = document.getElementById("cantidad").value;
+    let categoria = document.getElementById("categoria").value;
+    let total = calcular(cantidad, categoria); 
+    let texto = '<div class="alert alert-primary" role="alert">' +
+    'Total a Pagar: $ ' + total + '</div>';       
+    campoTotal.innerHTML = texto;
+}
+
 // calcular con funcion
-btnResumen.addEventListener("click", total = calcular(cantidad, categoria));
-
-// aplicar texto 
-
-texto.innerText =
-    <div class="alert alert-primary" role="alert">
-              Total a Pagar: ${total}
-    </div>;
-
-btnResumen.addEventListener("click", calculo.appendChild(texto));
+btnResumen.addEventListener('click', mostrar);
