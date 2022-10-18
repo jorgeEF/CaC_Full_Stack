481e0885e46ae503f15ffa76946eb75ce3140111
jorgeEF <jorgefemenia@gmail.com>
2022-10-18 10:31:54 -0300

    actualizado bucles javascript

--- 4.JAVASCRIPT/Ejercicios/js/script2.js
+++ 4.JAVASCRIPT/Ejercicios/js/script2.js
@@ -8,3 +8,16 @@ if (edad>=18){
 else {
     console.log("Es menor de edad")
 }
+// bucle while
+edad = 1;
+while (edad < 18){
+    console.log("es menor de edad, tiene ", edad, " años.")
+    edad++;
+}
+
+// bucle for
+
+for (let i = 0; i < 10; i++){
+    console.log(i);
+}
+
