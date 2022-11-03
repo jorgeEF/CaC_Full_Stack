f52420d452b86f7eef03328c6f75bde5a6bf8ee2
jorgeEF <jorgefemenia@gmail.com>
2022-11-03 11:26:22 -0300

    creado app.js y agregado al index

---
+++ 8.Node.js/ejercicios/app.js
@@ -0,0 +1,9 @@
+// aplicacion principal
+
+// importamos el modulo funciones
+
+const funciones = require("./funciones");
+
+// usamos la funcion sumar del modulo funciones
+
+console.log(funciones.sumar(15,40));
