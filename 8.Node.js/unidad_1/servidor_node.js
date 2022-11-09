ad93f3bb0c00274227802a8f3305eaff7d505a88
jorgeEF <jorgefemenia@gmail.com>
2022-11-08 23:05:41 -0300

    modificaciones en unidad node

--- 8.Node.js/unidad_1/servidor_node.js
+++
@@ -1,8 +0,0 @@
-// node crea un servidor http
-
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.write('Hello World!');
-  res.end();
-}).listen(8080);
