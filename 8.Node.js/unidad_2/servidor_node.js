938ba2347c3fd97ef83156e2b92aeb8aa66b9c55
jorgeEF <jorgefemenia@gmail.com>
2022-11-08 22:58:33 -0300

    modificaciones en unidad 2 de Node: servidor node

--- 8.Node.js/unidad_2/servidor_node.js
+++ 8.Node.js/unidad_2/servidor_node.js
@@ -6,8 +6,7 @@ const mensajes = function(request, response){
 	response.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
 	response.write("<h1>Este es el encabezado</h1>");
 	response.end("este es el ultimo mensaje");
-	console.log("ejecutando servidor en puerto 4000.")
 }
 // ejecutamos el servidor http de node con los mensajes de solicitud y respuesta en el puerto 4000.
 const servidor = http.createServer(mensajes);
-servidor.listen(4000);
+servidor.listen(4000, () => console.log("ejecutando servidor en http://localhost:4000."));
