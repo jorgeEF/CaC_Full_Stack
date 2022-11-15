829b1c0c2abc5943544e964e382026daafe611aa
jorgeEF <jorgefemenia@gmail.com>
2022-11-15 11:31:03 -0300

    agregada ruta a server_Express

--- 8.Node.js/unidad_4/server_express/app.js
+++ 8.Node.js/unidad_4/server_express/app.js
@@ -3,7 +3,14 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+// ruta raiz /
 app.get('/', (req,res) => {
 	res.send('hola')
 })
+
+// ruta consultas
+app.get('/consultas', (req,res) => {
+	res.send('Estas en consultas')
+})
+
 app.listen(PORT, () => console.log('Servidor corriendo desde: http://localhost:${PORT}'));
