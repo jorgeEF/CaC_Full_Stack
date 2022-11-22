87cdb4fb750d4da30c933dc4986dbb8a90d92ca9
jorgeEF <jorgefemenia@gmail.com>
2022-11-22 11:22:25 -0300

    express y express generator

--- 8.Node.js/unidad_4/server_express/app.js
+++ 8.Node.js/unidad_4/server_express/app.js
@@ -3,14 +3,32 @@
 const express = require('express');
 const app = express();
 const PORT = 3000;
+
+// set define una variable global disponible en toda la aplicación
+app.set("nombre", "Jorge");
+
+// middleware: se ejecuta ante cualquier petición y de ahi ejecuta el next
+app.use((req, res, next)=> {
+	console.log('estamos en el middleware')
+	next();
+});
+
 // ruta raiz /
 app.get('/', (req,res) => {
-	res.send('hola')
+	res.send('Hola, esta es la ruta raiz')
 })
 
-// ruta consultas
+// ruta consultas usando variable global "nombre"
 app.get('/consultas', (req,res) => {
-	res.send('Estas en consultas')
+	res.send('Estás en consultas ' + app.get("nombre"));
+})
+
+// ruta respuesta con json (api)
+app.post('/api', (req,res) => {
+	res.json([{"id": 1, "nombre": "Peine", "descripcion": "peine francés"}]);
 })
 
 app.listen(PORT, () => console.log(`Servidor corriendo desde: http://localhost:${PORT}`));
+
+// npm install nodemon
+// npx nodemon: ejecuta la app y la auto reinicia cuando hacemos cambios
