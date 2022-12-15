40ce36b161d25a73329aeabc2ca299e3af987491
jorgeEF <jorgefemenia@gmail.com>
2022-12-14 21:38:38 -0300

    terminado CRUD node/express

--- 8.Node.js/unidad_6/CRUD/routes/users.js
+++ 8.Node.js/unidad_6/CRUD/routes/users.js
@@ -12,7 +12,7 @@ var connection = mysql.createConnection({
 // se conecta a la base de datos
 connection.connect();
   
-// ruta raiz de users, muestra todos los usuarios.
+// muestra los usuarios con o sin filtro
 router.get('/', function(req = null, res, next) {
   let filtro = req.query.id;
   
@@ -30,10 +30,12 @@ router.get('/', function(req = null, res, next) {
   }      
 });
 
+// muestra pagina de creación de usuario
 router.get('/create', function(req, res, next) { 
     res.render('create', {title: 'Crear nuevo usuario'});
 });
 
+// guarda usuario creado
 router.post('/create', function(req, res, next) { 
   connection.query(`INSERT INTO users (nombre,apellido) VALUES('${req.body.nombre}','${req.body.apellido}');`, function(err, results, fields) {
     if (err) throw err;
@@ -41,13 +43,23 @@ router.post('/create', function(req, res, next) {
   });
 });
 
+// muestra usuario a editar
 router.get('/edit/:id', function(req, res, next) {  
-    connection.query('SELECT * FROM users WHERE id = ' + req.params.id, function(err, results, fields) {
+    connection.query('SELECT * FROM users WHERE id = ' + req.params.id, function(err, result, fields) {      
       if (err) throw err;      
-      res.render('users', { title: 'Usuarios', users: results});
+      res.render('edit', { title: 'Editar Usuario', user: result});
     });
 });
 
+// guarda usuario editado
+router.post('/edit/:id', function(req, res, next) {  
+  connection.query(`UPDATE users SET nombre = '${req.body.nombre}', apellido = '${req.body.apellido}' WHERE id = ` + req.params.id, function(err, result, fields) {      
+    if (err) throw err;      
+    res.redirect('/users');
+  });
+});
+
+// elimina usuario
 router.post('/delete/:id', function(req, res, next) {  
   connection.query('DELETE FROM users WHERE id = ' + req.params.id, function(err, results, fields) {
     if (err) throw err;
